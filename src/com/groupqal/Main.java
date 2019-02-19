9be46467c0cd4dd0167fb1dc3607e55fe8d50286
pushkelya <pushkelya.al@gmail.com>
2019-02-19 12:11:19 +0200

    hw1

---
+++ src/com/groupqal/Main.java
@@ -0,0 +1,12 @@
+package com.groupqal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("****  ****    *      *       *");
+        System.out.println("*     *  *   * *     * *   * *");
+        System.out.println("****  ****  *   *    *  * *  *");
+        System.out.println("*     *    *******   *   *   *");
+        System.out.println("****  *   *       *  *       *");
+    }
+}
